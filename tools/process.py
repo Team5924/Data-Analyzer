5b325a3278b82528127a1d9381dd400673b1fc8f
cyclo9 <liang.aidan139@gmail.com>
2022-10-20 14:20:20 -0700

    Finish 'process.py'

--- tools/process.py
+++ tools/process.py
@@ -2,8 +2,8 @@ import numpy as np
 
 # Removes the header from each entry
 # "s=0" --> 0
+# 'input' --> String
 
-# "input" --> String
 def entry(input, dtype):
     # Locates the index of the "="
     x = 0
@@ -13,19 +13,19 @@ def entry(input, dtype):
             break
         x = x+1
     m_entry = []
-    # Selects the data from the entry, excluding the header
+    # Extracts the data from 'input'; excludes the header
     for i in range(x, len(input)):
         m_entry.append(input[i])
-    # Type = True --> quantitative data; Type = False --> qualitative data
-    if not type:
+    # 'dType' = True --> int; 'dType' = False --> string
+    if not dtype:
         return "".join(m_entry)
     else:
         return int("".join(m_entry))
 
-# Utilizes the entry() to process and create a new array of data without the header
+# Utilizes the 'entry()' to process and create a new array of data without the header
 
 def array(arr):
-    m_array = np.empty(np.shape(arr), np.int8)
+    m_array = np.zeros(np.shape(arr), np.int8)
     for m in range(len(arr)):
         for n in range(len(arr[m])):
             m_array[m][n] = entry(arr[m][n], True)
