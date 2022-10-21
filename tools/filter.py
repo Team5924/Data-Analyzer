dd6ff085f04c883fb9444b0bf8591af807ff6f2d
cyclo9 <32120227+Cyclo9@users.noreply.github.com>
2022-10-20 17:35:23 -0700

    Improved filter functions to exclude 0's rows

--- tools/filter.py
+++ tools/filter.py
@@ -3,24 +3,24 @@ import numpy as np
 # Makes a copy of 'arr' and replaces each row with only QT data
 
 def qt(arr):
-    m_array = np.zeros(np.shape(arr), np.int8)
+    m_array = np.zeros(np.shape(arr), np.int64)
     x = 0
     for m in range(len(arr)):
         if arr[m][0] == 0: # 'h=0' --> quantitative
             m_array[x] = arr[m]
             x = x + 1
-    return m_array
+    return m_array[~np.all(m_array == 0, axis=1)]
 
 # Makes a copy of 'arr' and replaces each row with only QL data
 
 def ql(arr):
-    m_array = np.zeros(np.shape(arr), np.int8)
+    m_array = np.zeros(np.shape(arr), np.int64)
     x = 0
     for m in range(len(arr)):
         if arr[m][0] == 1: # 'h=1' --> qualitative
             m_array[x] = arr[m]
             x = x + 1
-    return m_array
+    return m_array[~np.all(m_array == 0, axis=1)]
 
 # Makes a copy of 'arr' and replaces each row with only data from 'team'
 # 'column' is the index of 'team' in 'arr'
@@ -32,7 +32,7 @@ def team(arr, team, column):
         if arr[m][column] == team:
             m_array[x] = arr[m]
             x = x + 1
-    return m_array
+    return m_array[~np.all(m_array == 0, axis=1)]
 
 # Makes a copy of 'arr' and replaces each row with only data from 'match'
 # 'column' is the index of 'match' in 'arr'
@@ -44,4 +44,4 @@ def match(arr, match, column):
         if arr[m][column] == match:
             m_array[x] = arr[m]
             x = x + 1
-    return m_array
+    return m_array[~np.all(m_array == 0, axis=1)]
