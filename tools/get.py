4ed214d9f8139d1f6a185ca8a6ca8e9f7d54a962
cyclo9 <32120227+Cyclo9@users.noreply.github.com>
2022-11-09 11:50:34 -0800

    Complete picklist

--- tools/get.py
+++ tools/get.py
@@ -6,3 +6,13 @@ def alliance(arr, team, team_index, match, match_index):
     for i in range(len(m_array)):
         if m_array[i][match_index] == match:
             return m_array[i][3] # '3' is the index of alliance
+
+# filters out teams that actually have data
+def teams(arr, team_index, team_list):
+    teams = []
+    for i in range(len(team_list)):
+        for j in range(len(arr)):
+            if arr[j][team_index] == team_list[i][0]:
+                teams.append(team_list[i][0])
+                break
+    return teams
