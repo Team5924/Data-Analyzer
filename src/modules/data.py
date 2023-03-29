33f94fd10838e2eef481f5583d00a363a3c9e67d
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2023-03-29 01:52:44 -0700

    Clean some files

--- src/modules/data.py
+++ src/modules/data.py
@@ -1,14 +1,17 @@
 raw_data = [
-    # ID2-M46-5924-B1
-    {"id":2,"match":46,"team":5924,"alliance": 0,"attendance":1,"mobility":1,"autoDocked":0,"autoEngaged":0,"autoScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":0}},"teleopScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":2}},"links":5,"piecesDropped":0,"status":"Operational","defense":"N/A","sabotage":0,"parked":1,"endgameDocked":0,"endgameEngaged":0,"notes":""},
-    # ID2-M46-114-B2
-    {"id":2,"match":46,"team":114,"alliance": 1,"attendance":1,"mobility":1,"autoDocked":0,"autoEngaged":0,"autoScore":{"top":{"cones":0,"cubes":1},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":1}},"teleopScore":{"top":{"cones":1,"cubes":1},"mid":{"cones":2,"cubes":0},"bot":{"cones":1,"cubes":1}},"links":5,"piecesDropped":0,"status":"Operational","defense":"N/A","sabotage":0,"parked":0,"endgameDocked":1,"endgameEngaged":1,"notes":""},
-    # ID2-M46-5419-B3
-    {"id":2,"match":46,"team":5419,"alliance": 2,"attendance":1,"mobility":1,"autoDocked":1,"autoEngaged":1,"autoScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":0}},"teleopScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":1,"cubes":2},"bot":{"cones":0,"cubes":4}},"links":5,"piecesDropped":0,"status":"Operational","defense":"N/A","sabotage":0,"parked":0,"endgameDocked":1,"endgameEngaged":1,"notes":""},
-    # ID2-M46-4186-R1
-    {"id":2,"match":46,"team":4186,"alliance": 3,"attendance":1,"mobility":0,"autoDocked":0,"autoEngaged":0,"autoScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":0}},"teleopScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":1}},"links":0,"piecesDropped":0,"status":"Immobile","defense":"N/A","sabotage":0,"parked":0,"endgameDocked":0,"endgameEngaged":0,"notes":""},
-    # ID2-M46-8169-R2
-    {"id":2,"match":46,"team":8169,"alliance": 4,"attendance":1,"mobility":1,"autoDocked":0,"autoEngaged":0,"autoScore":{"top":{"cones":0,"cubes":1},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":0}},"teleopScore":{"top":{"cones":1,"cubes":1},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":2}},"links":0,"piecesDropped":1,"status":"Operational","defense":"N/A","sabotage":0,"parked":0,"endgameDocked":1,"endgameEngaged":1,"notes":""},
-    # ID2-M46-6269-R3
-    {"id":2,"match":46,"team":6962,"alliance": 5,"attendance":1,"mobility":0,"autoDocked":1,"autoEngaged":1,"autoScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":1}},"teleopScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":1}},"links":0,"piecesDropped":0,"status":"Operational","defense":"1 (Terrible)","sabotage":0,"parked":0,"endgameDocked":1,"endgameEngaged":1,"notes":""},
+    
+
+    # Test Data
+    # # ID2-M46-5924-B1
+    # {"id":2,"match":46,"team":5924,"alliance": 0,"attendance":1,"mobility":1,"autoDocked":0,"autoEngaged":0,"autoScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":0}},"teleopScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":2}},"links":5,"piecesDropped":0,"status":"Operational","defense":"N/A","sabotage":0,"parked":1,"endgameDocked":0,"endgameEngaged":0,"notes":""},
+    # # ID2-M46-114-B2
+    # {"id":2,"match":46,"team":114,"alliance": 1,"attendance":1,"mobility":1,"autoDocked":0,"autoEngaged":0,"autoScore":{"top":{"cones":0,"cubes":1},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":1}},"teleopScore":{"top":{"cones":1,"cubes":1},"mid":{"cones":2,"cubes":0},"bot":{"cones":1,"cubes":1}},"links":5,"piecesDropped":0,"status":"Operational","defense":"N/A","sabotage":0,"parked":0,"endgameDocked":1,"endgameEngaged":1,"notes":""},
+    # # ID2-M46-5419-B3
+    # {"id":2,"match":46,"team":5419,"alliance": 2,"attendance":1,"mobility":1,"autoDocked":1,"autoEngaged":1,"autoScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":0}},"teleopScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":1,"cubes":2},"bot":{"cones":0,"cubes":4}},"links":5,"piecesDropped":0,"status":"Operational","defense":"N/A","sabotage":0,"parked":0,"endgameDocked":1,"endgameEngaged":1,"notes":""},
+    # # ID2-M46-4186-R1
+    # {"id":2,"match":46,"team":4186,"alliance": 3,"attendance":1,"mobility":0,"autoDocked":0,"autoEngaged":0,"autoScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":0}},"teleopScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":1}},"links":0,"piecesDropped":0,"status":"Immobile","defense":"N/A","sabotage":0,"parked":0,"endgameDocked":0,"endgameEngaged":0,"notes":""},
+    # # ID2-M46-8169-R2
+    # {"id":2,"match":46,"team":8169,"alliance": 4,"attendance":1,"mobility":1,"autoDocked":0,"autoEngaged":0,"autoScore":{"top":{"cones":0,"cubes":1},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":0}},"teleopScore":{"top":{"cones":1,"cubes":1},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":2}},"links":0,"piecesDropped":1,"status":"Operational","defense":"N/A","sabotage":0,"parked":0,"endgameDocked":1,"endgameEngaged":1,"notes":""},
+    # # ID2-M46-6269-R3
+    # {"id":2,"match":46,"team":6962,"alliance": 5,"attendance":1,"mobility":0,"autoDocked":1,"autoEngaged":1,"autoScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":1}},"teleopScore":{"top":{"cones":0,"cubes":0},"mid":{"cones":0,"cubes":0},"bot":{"cones":0,"cubes":1}},"links":0,"piecesDropped":0,"status":"Operational","defense":"1 (Terrible)","sabotage":0,"parked":0,"endgameDocked":1,"endgameEngaged":1,"notes":""},
 ]
